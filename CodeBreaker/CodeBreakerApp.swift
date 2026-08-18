752c9f651d969fb827ff85c3b103234e31b15946
WeiAnUppp <weianuen@icloud.com>
2026-08-18 16:48:54 +0800

    Initial Commit

---
+++ CodeBreaker/CodeBreakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeBreakerApp.swift
+//  CodeBreaker
+//
+//  Created by HUAWEI MateBook X on 2026/8/18.
+//
+
+import SwiftUI
+
+@main
+struct CodeBreakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
