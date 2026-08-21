eb4af95cc18c36f47a68e689b85398bb80d7d68e
WeiAnUppp <weianuen@icloud.com>
2026-08-21 17:51:19 +0800

    L5 Layout & DataFlow

--- CodeBreaker/CodeBreaker.swift
+++ CodeBreaker/CodeBreaker.swift
@@ -33,7 +33,7 @@ struct CodeBreaker{
             let newPeg = pegChoices[(indexOfExistingPegInPegChoices + 1) % pegChoices.count]
             guess.pegs[index] = newPeg
         }else{
-            guess.pegs[index] = pegChoices.first ?? Code.missing
+            guess.pegs[index] = pegChoices.first ?? Code.missingPeg
         }
     }
     
@@ -42,9 +42,9 @@ struct CodeBreaker{
 
 struct Code{
     var kind: Kind
-    var pegs: [Peg] = Array(repeating: Code.missing, count: 4)
+    var pegs: [Peg] = Array(repeating: Code.missingPeg, count: 4)
     
-    static let missing: Peg = .clear
+    static let missingPeg: Peg = .clear
     
     enum Kind: Equatable{
         case master
@@ -55,38 +55,39 @@ struct Code{
     
    mutating func randomize(from pegChoices: [Peg]){
         for index in pegChoices.indices{
-            pegs[index] = pegChoices.randomElement() ?? Code.missing
+            pegs[index] = pegChoices.randomElement() ?? Code.missingPeg
         }
     }
     
-    var matches: [Match]{
+    var matches: [Match]? {
         switch kind{
         case .attempt(let matches): return matches
-        default: return []
+        default: return nil
         }
     }
     
     func match(against otherCode: Code) -> [Match] {
-        var results: [Match] = Array(repeating: .nomatch, count: pegs.count)
         var pegsToMatch = otherCode.pegs
-        
-        for index in pegs.indices.reversed() {
-            if results[index] != .exact, pegsToMatch[index] == pegs[index] {
-                results[index] = .exact
+
+        let backwardsExactMatches = pegs.indices.reversed().map { index in
+            if pegsToMatch.count > index, pegsToMatch[index] == pegs[index] {
                 pegsToMatch.remove(at: index)
+                return Match.exact
+            } else {
+                return .nomatch
             }
         }
         
-        for index in pegs.indices {
-            if results[index] != .exact {
-                if let matchIndex = pegsToMatch.firstIndex(of: pegs[index]) {
-                    results[index] = .inexact
-                    pegsToMatch.remove(at: matchIndex)
-                }
+        let exactMatches = Array(backwardsExactMatches.reversed())
+
+        return pegs.indices.map { index in
+            if exactMatches[index] != .exact, let matchIndex = pegsToMatch.firstIndex(of: pegs[index]) {
+                pegsToMatch.remove(at: matchIndex)
+                return .inexact
+            } else {
+                return exactMatches[index]
             }
         }
-        
-        return results
     }
 
 }
--- CodeBreaker/CodeBreakerView.swift
+++ CodeBreaker/CodeBreakerView.swift
@@ -40,7 +40,7 @@ struct CodeBreakerView: View {
             ForEach(code.pegs.indices, id: \.self) { index in 
                 RoundedRectangle(cornerRadius: 10)
                     .overlay{
-                        if code.pegs[index] == Code.missing{
+                        if code.pegs[index] == Code.missingPeg{
                             RoundedRectangle(cornerRadius: 10)
                                 .strokeBorder(Color.gray)
                         }
@@ -54,10 +54,16 @@ struct CodeBreakerView: View {
                         }
                     }
             }
-            MatchMarker(matches: code.matches)
+            Rectangle()
+                .foregroundStyle(.clear)
+                .aspectRatio(1, contentMode: .fit)
                 .overlay{
-                    if code.kind == .guess{
-                        guessButton
+                    if let matches = code.matches {
+                        MatchMarker(matches: matches)
+                    }else{
+                        if code.kind == .guess{
+                            guessButton
+                        }
                     }
                 }
         }
